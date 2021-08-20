e2cbb19cbcf15202bd52c0c1313fb2be96ddde59
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2021-08-20 14:59:08 +0200

    Implemented logical and aretmetic negation prefix operators

--- LentoCore/Expressions/Prefix.cs
+++ LentoCore/Expressions/Prefix.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LentoCore.Exception;
 using LentoCore.Parser;
 using LentoCore.Util;
 
@@ -22,7 +23,22 @@ namespace LentoCore.Expressions
 
         public override Atomic Evaluate()
         {
-            throw new NotImplementedException();
+            Atomic value = _rhs.Evaluate();
+            switch (_operator)
+            {
+                case PrefixOperator.Negative:
+                {
+                    if (value is Integer @integer) return new Atoms.Integer(@integer.Value * -1);
+                    if (value is Float @float) return new Atoms.Float(@float.Value * -1);
+                    throw new EvaluateErrorException(ErrorHandler.EvaluateErrorTypeMismatch(_rhs.Span.Start, value, typeof(Integer), typeof(Float)));
+                }
+                case PrefixOperator.Not:
+                {
+                    if (value is Atoms.Boolean @bool) return new Atoms.Boolean(!@bool.Value);
+                    throw new EvaluateErrorException(ErrorHandler.EvaluateErrorTypeMismatch(_rhs.Span.Start, value, typeof(Atoms.Boolean)));
+                }
+                default: throw new EvaluateErrorException(ErrorHandler.EvaluateError(Span.Start, $"Could not evaluate {_operator}. Invalid prefix operator!"));
+            }
         }
 
         public override string ToString() => $"{_operator}({_rhs.ToString()})";
