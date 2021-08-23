88996220010420d07fb6fc65e88e4f908652b362
WilliamRagstad <41281398+WilliamRagstad@users.noreply.github.com>
2021-08-23 16:43:37 +0200

    Validate files in interpreter

--- LentoCLI/Interpreter.cs
+++ LentoCLI/Interpreter.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LentoCLI.Util;
+using LentoCore.Evaluator;
 
 namespace LentoCLI
 {
     public static class Interpreter
     {
         public static void Run(string[] files)
-        {
+        {            
+            foreach(string file in files)
+            {
+                if (FileHelper.Validate(file))
+                {
 
+                    Evaluator.Evaluate();
+                }
+            }
         }
     }
 }
