4b4c28677ee9fc838e2039509cd0daee0faeb802
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2021-08-23 16:44:24 +0200

    Validate file helper

---
+++ LentoCLI/Util/FileHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LentoCLI.Util
+{
+    public static class FileHelper
+    {
+        public static bool Validate(string file)
+        {
+            if (!File.Exists(file))
+            {
+                Console.WriteLine($"File '{file}' does not exist!");
+                return false;
+            }
+            else if (Directory.Exists(file))
+            {
+                Console.WriteLine($"Cannot open directory '{file}' as file!");
+                return false;
+            }
+            return true;
+        }
+    }
+}
