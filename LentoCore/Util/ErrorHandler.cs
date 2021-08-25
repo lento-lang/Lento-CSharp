d70f265fa8a8a3a3c5c63c5c987bdb04d100b887
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2021-08-25 18:32:31 +0200

    Update ErrorHandler.cs

--- LentoCore/Util/ErrorHandler.cs
+++ LentoCore/Util/ErrorHandler.cs
@@ -12,9 +12,13 @@ namespace LentoCore.Util
         public static string SyntaxError(LineColumn position, string message) => $"Syntax error at line {position.Line} column {position.Column}: {message}";
         public static string ParseError(LineColumn position, string message) => $"Parse error at line {position.Line} column {position.Column}: {message}";
         public static string TypeError(LineColumn position, string message) => $"Type error at line {position.Line} column {position.Column}: {message}";
-        public static string EvaluateError(LineColumn position, string message) => $"Evaluation runtime error at line {position.Line} column {position.Column}: {message}";
-
+        public static string EvaluateError(LineColumn position, string message) => $"Runtime error at line {position.Line} column {position.Column}: {message}";
+        
         public static string EvaluateErrorTypeMismatch(LineColumn position, Atomic got, params Type[] expectedTypes) =>
             EvaluateError(position, $"Type mismatch. Expected {Formatting.FormattableOptionsToString(expectedTypes.Select(et => et.Name))} but got '{got.ToString()}' of type {got.GetType().Name}");
+        public static string EvaluateErrorTypeMismatch(LineColumn position, string inOp, Atomic got, params Type[] expectedTypes) =>
+            EvaluateError(position, $"Type mismatch in {inOp} operation. Expected {Formatting.FormattableOptionsToString(expectedTypes.Select(et => et.Name))} but got '{got.ToString()}' of type {got.GetType().Name}");
+        public static string EvaluateErrorTypeMismatch(LineColumn position, Atomic got, string expected) =>
+            EvaluateError(position, $"Type mismatch. Expected {expected} but got '{got.ToString()}' of type {got.GetTypeName()}");
     }
 }
