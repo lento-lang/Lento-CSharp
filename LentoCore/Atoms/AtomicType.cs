4632002990f64cbdb89afef2794ec5311e001074
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2021-08-26 10:06:16 +0200

    Decouple Identifier and AtomicType circular dependency bug

--- LentoCore/Atoms/AtomicType.cs
+++ LentoCore/Atoms/AtomicType.cs
@@ -6,17 +6,19 @@ using System.Threading.Tasks;
 
 namespace LentoCore.Atoms
 {
-    public class AtomicType : Identifier
+    public class AtomicType : Atomic
     {
+        public string Name;
         private readonly string _stringRepresentation;
         public AtomicType(string name) : this(name, string.Empty) { }
-        public AtomicType(string name, string stringRepresentation) : base(name)
+        public AtomicType(string name, string stringRepresentation)
         {
+            Name = name;
             _stringRepresentation = stringRepresentation;
         }
         public virtual bool Equals(AtomicType other) => Name.Equals(other.Name);
 
         public override string ToString() =>
-            _stringRepresentation != string.Empty ? _stringRepresentation : base.ToString();
+            _stringRepresentation != string.Empty ? _stringRepresentation : Name;
     }
 }
