b1a66d517e3e91849994257d6d41d61a9c8100c5
WilliamRagstad <41281398+WilliamRagstad@users.noreply.github.com>
2021-09-20 17:25:02 +0200

    Empty code block

--- LentoCore/Expressions/Block.cs
+++ LentoCore/Expressions/Block.cs
@@ -25,7 +25,7 @@ namespace LentoCore.Expressions
             return result;
         }
 
-        public override AtomicType GetReturnType(TypeTable table) => _expressions.LastOrDefault().GetReturnType(table);
-        public override string ToString(string indent) => $"{{\n{indent + Formatting.Indentation}{string.Join(indent + Formatting.Indentation, _expressions.Select(e => e.ToString(indent + Formatting.Indentation)))}\n{indent}}}";
+        public override AtomicType GetReturnType(TypeTable table) => _expressions.Length > 0 ? _expressions.Last().GetReturnType(table) : Unit.BaseType;
+        public override string ToString(string indent) => _expressions.Length > 0 ? $"{{\n{indent + Formatting.Indentation}{string.Join(indent + Formatting.Indentation, _expressions.Select(e => e.ToString(indent + Formatting.Indentation)))}\n{indent}}}" : "{ }";
     }
 }
